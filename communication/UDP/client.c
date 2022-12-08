6f3ba456baaebf3b215f08620f45e6111f702bf3
MohamedAmineBenAmmar <ma.benammar5@gmail.com>
2022-12-08 18:24:44 +0100

    udp logic is fine

--- communication/UDP/client.c
+++ communication/UDP/client.c
@@ -1,8 +1,11 @@
 #include "../../global/libs.h"
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
+  int x;
 
-  if (argc != 2) {
+  if (argc != 2)
+  {
     printf("Usage: %s <port>\n", argv[0]);
     exit(0);
   }
@@ -11,7 +14,7 @@ int main(int argc, char **argv){
   int port = atoi(argv[1]);
 
   int sockfd;
-  struct sockaddr_in addr;
+  struct sockaddr_in addr, ackaddr;
   char buffer[1024];
   socklen_t addr_size;
 
@@ -21,15 +24,27 @@ int main(int argc, char **argv){
   addr.sin_port = htons(port);
   addr.sin_addr.s_addr = inet_addr(ip);
 
+  memset(&ackaddr, '\0', sizeof(ackaddr));
+  ackaddr.sin_family = AF_INET;
+  ackaddr.sin_port = htons(3000);
+  ackaddr.sin_addr.s_addr = inet_addr(ip);
+
   bzero(buffer, 1024);
   strcpy(buffer, "Hello, World!");
-  sendto(sockfd, buffer, 1024, 0, (struct sockaddr*)&addr, sizeof(addr));
+  sendto(sockfd, buffer, 1024, 0, (struct sockaddr *)&addr, sizeof(addr));
   printf("[+]Data send: %s\n", buffer);
 
   bzero(buffer, 1024);
   addr_size = sizeof(addr);
-  recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)&addr, &addr_size);
+  recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr *)&addr, &addr_size);
   printf("[+]Data recv: %s\n", buffer);
+
+  //  printf("the new socket created by the server :%d\n", ntohs(tmpaddr.sin_port));
+  printf("the old socket created by the server :%d\n", ntohs(addr.sin_port));
+
+  printf("da5el value\n");
+  scanf("%d", &x);
+  sendto(sockfd, "yoooo hhh", 1024, 0, (struct sockaddr *)&ackaddr, sizeof(ackaddr));
 
   return 0;
 }
--- communication/UDP/server.c
+++ communication/UDP/server.c
@@ -1,8 +1,10 @@
 #include "../../global/libs.h"
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
 
-  if (argc != 2){
+  if (argc != 2)
+  {
     printf("Usage: %s <port>\n", argv[0]);
     exit(0);
   }
@@ -11,13 +13,26 @@ int main(int argc, char **argv){
   int port = atoi(argv[1]);
 
   int sockfd;
-  struct sockaddr_in server_addr, client_addr;
+  int acksockfd;
+
+  int childs = 0;
+
+  struct sockaddr_in server_addr, client_addr, server_ack_addr, client_ack_addr;
   char buffer[1024];
-  socklen_t addr_size;
+  socklen_t addr_size, client_ack_addr_size;
   int n;
+  pid_t childpid;
 
   sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (sockfd < 0){
+  if (sockfd < 0)
+  {
+    perror("[-]socket error");
+    exit(1);
+  }
+
+  acksockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (acksockfd < 0)
+  {
     perror("[-]socket error");
     exit(1);
   }
@@ -27,21 +42,47 @@ int main(int argc, char **argv){
   server_addr.sin_port = htons(port);
   server_addr.sin_addr.s_addr = inet_addr(ip);
 
-  n = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if (n < 0) {
+  memset(&server_ack_addr, '\0', sizeof(server_ack_addr));
+  server_ack_addr.sin_family = AF_INET;
+  server_ack_addr.sin_port = htons(3000);
+  server_ack_addr.sin_addr.s_addr = inet_addr(ip);
+
+  n = bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (n < 0)
+  {
     perror("[-]bind error");
     exit(1);
   }
 
-  bzero(buffer, 1024);
-  addr_size = sizeof(client_addr);
-  recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)&client_addr, &addr_size);
-  printf("[+]Data recv: %s\n", buffer);
+  n = bind(acksockfd, (struct sockaddr *)&server_ack_addr, sizeof(server_ack_addr));
+  if (n < 0)
+  {
+    perror("[-]bind error");
+    exit(1);
+  }
 
-  bzero(buffer, 1024);
-  strcpy(buffer, "Welcome to the UDP Server.");
-  sendto(sockfd, buffer, 1024, 0, (struct sockaddr*)&client_addr, sizeof(client_addr));
-  printf("[+]Data send: %s\n", buffer);
+  while (1)
+  {
+    bzero(buffer, 1024);
+    addr_size = sizeof(client_addr);
+    recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr *)&client_addr, &addr_size);
+    printf("[+]Data recv: %s\n", buffer);
+
+    if ((childpid = fork()) == 0)
+    {
+
+      printf("Im communicting with \n");
+      printf("Data : adress %s port: %d.\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+      bzero(buffer, 1024);
+      strcpy(buffer, "Welcome to the UDP Server.");
+      sendto(sockfd, buffer, 1024, 0, (struct sockaddr *)&client_addr, sizeof(client_addr));
+      printf("[+]Data send: %s\n", buffer);
+
+      recvfrom(acksockfd, buffer, 1024, 0, (struct sockaddr *)&client_ack_addr, &client_ack_addr_size);
+      printf("jeni ack men 3and \n");
+      printf("Data : adress %s port: %d\n", inet_ntoa(client_ack_addr.sin_addr), ntohs(client_ack_addr.sin_port));
+    }
+  }
 
   return 0;
 }
