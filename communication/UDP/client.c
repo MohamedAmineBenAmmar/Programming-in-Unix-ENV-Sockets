89d91cfe1c4391fa2f601101d424e6abc1923644
MohamedAmineBenAmmar <ma.benammar5@gmail.com>
2022-12-08 14:44:22 +0100

    fix udp communication

--- communication/UDP/client.c
+++ communication/UDP/client.c
@@ -1,71 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-struct Person
-{
-    /* data */
-    char name[50];
-};
+int main(int argc, char **argv){
 
-int main (int argc, char **argv){
-    // Test
-    struct Person p1;
-    strcpy(p1.name, "Hamma is the best");
-    // end test
-    
-    // Arguments validation
-    if(argc != 2){
-        printf("Usage error: You must specify a port number\n");
-        exit(0);
-    }
+  if (argc != 2) {
+    printf("Usage: %s <port>\n", argv[0]);
+    exit(0);
+  }
 
-    // Setting the ip address manually
-    char *ip = "127.0.0.1";
+  char *ip = "127.0.0.1";
+  int port = atoi(argv[1]);
 
-    // Getting the port number from the command line as argument
-    int port = atoi(argv[1]);
+  int sockfd;
+  struct sockaddr_in addr;
+  char buffer[1024];
+  socklen_t addr_size;
 
-    // Init the needed variables
-    int socketfd;
-    struct sockaddr_in addr;
-    char buffer[1024];
-    socklen_t addr_size;
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  memset(&addr, '\0', sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(port);
+  addr.sin_addr.s_addr = inet_addr(ip);
 
-    // Initializing the socket (UDP)
-    socketfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if(socketfd < 0){
-        perror("[-]socket error\n");
-        exit(1);
-    }
+  bzero(buffer, 1024);
+  strcpy(buffer, "Hello, World!");
+  sendto(sockfd, buffer, 1024, 0, (struct sockaddr*)&addr, sizeof(addr));
+  printf("[+]Data send: %s\n", buffer);
 
-    // Setting some values in the client socket
-    memset(&addr, '\0', sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = inet_addr(ip);
+  bzero(buffer, 1024);
+  addr_size = sizeof(addr);
+  recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)&addr, &addr_size);
+  printf("[+]Data recv: %s\n", buffer);
 
-    // Sending data from the client to the server
-    bzero(buffer, 1024);
-    
-    strcpy(buffer, "yo from the shity client");
-    // sendto(socketfd, buffer, 1024, 0, (struct sockaddr*)&addr, sizeof(addr));
-    // printf("[+]Data sent to the server %s\n", buffer);
-    sendto(socketfd, &p1, sizeof(struct Person), 0, (struct sockaddr*)&addr, sizeof(addr));
-    printf("[+]Data sent to the server %s\n", p1.name);
-
-
-    // Receving response from the server
-    bzero(buffer, 1024);
-    addr_size = sizeof(addr);
-    recvfrom(socketfd, buffer, 1024, 0, (struct sockaddr*)&addr, &addr_size);
-    printf("[+]Data received from the server %s\n", buffer);
-
-    close(socketfd);
-    return 0;
+  return 0;
 }
--- communication/UDP/server.c
+++ communication/UDP/server.c
@@ -1,88 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+int main(int argc, char **argv){
 
-struct Person
-{
-    /* data */
-    char name[50];
-};
+  if (argc != 2){
+    printf("Usage: %s <port>\n", argv[0]);
+    exit(0);
+  }
 
-int main(int argc, char **argv)
-{
-    // Test
-    struct Person p1;
-    // strcpy(p1.name, "Hamma is the best");
-    // end test
+  char *ip = "127.0.0.1";
+  int port = atoi(argv[1]);
 
-    // Arguments validation
-    if (argc != 2)
-    {
-        printf("Usage error: You must specify a port number\n");
-        exit(0);
-    }
+  int sockfd;
+  struct sockaddr_in server_addr, client_addr;
+  char buffer[1024];
+  socklen_t addr_size;
+  int n;
 
-    // Setting the ip address manually
-    char *ip = "127.0.0.1";
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0){
+    perror("[-]socket error");
+    exit(1);
+  }
 
-    // Getting the port number from the command line as argument
-    int port = atoi(argv[1]);
+  memset(&server_addr, '\0', sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  server_addr.sin_addr.s_addr = inet_addr(ip);
 
-    // Init the needed variables
-    int socketfd;
-    struct sockaddr_in server_addr, client_addr;
-    char buffer[1024];
-    socklen_t addr_size;
-    int n;
+  n = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+  if (n < 0) {
+    perror("[-]bind error");
+    exit(1);
+  }
 
-    // Initializing the socket (UDP)
-    socketfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (socketfd < 0)
-    {
-        perror("[-]socket error\n");
-        exit(1);
-    }
+  bzero(buffer, 1024);
+  addr_size = sizeof(client_addr);
+  recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)&client_addr, &addr_size);
+  printf("[+]Data recv: %s\n", buffer);
 
-    // Setting some values in the server socket
-    memset(&server_addr, '\0', sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
-    server_addr.sin_addr.s_addr = inet_addr(ip);
+  bzero(buffer, 1024);
+  strcpy(buffer, "Welcome to the UDP Server.");
+  sendto(sockfd, buffer, 1024, 0, (struct sockaddr*)&client_addr, sizeof(client_addr));
+  printf("[+]Data send: %s\n", buffer);
 
-    // Setting the bind
-    n = bind(socketfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    if (n < 0)
-    {
-        perror("[-]bind error\n");
-        exit(1);
-    }
-
-    while (1)
-    {
-        // Listening to upcomong requests
-        printf("Server is listening at port: %d\n", port);
-
-        // Receiving data from the client
-        bzero(buffer, 1024);
-        addr_size = sizeof(client_addr);
-        // recvfrom(socketfd, buffer, 1024, 0, (struct sockaddr *)&client_addr, &addr_size);
-        // printf("[+]Data received from the client %s\n", buffer);
-        recvfrom(socketfd, &p1, sizeof(struct Person), 0, (struct sockaddr *)&client_addr, &addr_size);
-        printf("[+]Data received from the client %s\n", p1.name);
-
-        // Send the data back to the client
-        bzero(buffer, 1024);
-        strcpy(buffer, "UDP server bankai");
-        sendto(socketfd, buffer, 1024, 0, (struct sockaddr *)&client_addr, sizeof(client_addr));
-        printf("[+]Data sent to the client %s\n", buffer);
-    }
-
-    close(socketfd);
-    return 0;
+  return 0;
 }
